e0466c8a1874e4f42f007d4448126a57bc675332
LeTrongThe <theltgcc200111@fpt.edu.vn>
2023-06-22 18:55:30 +0700

    upp

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
-    $conn = mysqli_connect('localhost','root','','atndb') 
-    or die("Cannot connect database".mysqli_connect_error());
+ $conn = mysqli_connect('us-cdbr-east-06.cleardb.net','bc6d4e2b6f09ea','0df41248','heroku_09473510bd4ce38') 
+ or die("Cannot connect database".mysqli_connect_error());
 
     // $conn = mysqli_connect("nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","im6uv6tb0uzh0kef","b4ihu2lkxh8ho1b2","mhzmhjrrq860l1d5") 
     // or die("Cannot connect database".mysqli_connect_error());
