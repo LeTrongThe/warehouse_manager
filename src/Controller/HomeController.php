fae9dd64c2e7613375f6cbd23ffa6e31319af325
LeTrongThe <theltgcc200111@fpt.edu.vn>
2023-06-24 18:03:41 +0700

    upp

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Repository\ProductRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,10 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
+     public function x(ProductRepository $productRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'products' => $productRepository->findAll(),
+        ]);
+    }
+
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+    
 }
